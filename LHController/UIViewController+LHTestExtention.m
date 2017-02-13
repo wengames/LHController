e05b3f96d2dc30d85597cb1c77d87a6b0ab12c26
刘海文 <liuhaiwen@wondersgroup.com>
2017-02-13 13:25:27 +0800

    Release 1.0.0.

---
+++ LHController/UIViewController+LHTestExtention.m
@@ -0,0 +1,17 @@
+//
+//  UIViewController+LHTestExtention.m
+//  LHControllerDemo
+//
+//  Created by admin on 2017/2/13.
+//  Copyright © 2017年 刘海文. All rights reserved.
+//
+
+#import "UIViewController+LHTestExtention.h"
+
+@implementation UIViewController (LHTestExtention)
+
++ (void)load {
+    NSLog(@"LHController works!");
+}
+
+@end
